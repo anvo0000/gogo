80e2d049b27df052492d5d06d597204165e3f8bd
An Vo <an.vo@adurolife.com>
2024-07-02 11:46:37 +0700

    add requirement.txt

--- hello.py
+++ hello.py
@@ -1,5 +1,18 @@
+import os
+import openai
+import langchain
+import huggingface_hub
+import transformers
+import chromadb
+
+
 def main():
-    print("hello abc dddef")
+    print("hello abc def")
+    print(f"OpenAI version: {openai.version.__version__}")
+    print(f"Langchain: {langchain.__version__}")
+    print(f"HuggingFace hub: {huggingface_hub.__version__}")
+    print(f"Transformer: {transformers.__version__}")
+    print(f"ChromaDb: {chromadb.__version__}")
 
 
 if __name__ == '__main__':
